552d42b7cdbfdf078550970dd9ecd63368319947
sunilkumarhs <sunilkumarhs974117@gmail.com>
2024-12-02 23:42:56 +0530

    first-commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,9 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          "min-h-screen antialiased grainy",
+          `${geistSans.variable} ${geistMono.variable} antialiased`
+        )}
       >
         {children}
       </body>
