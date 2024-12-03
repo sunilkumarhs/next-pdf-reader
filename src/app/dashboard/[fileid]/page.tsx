b4eb6c96fdb3a62d1f3d45b6514fbc56e2509caa
sunilkumarhs <sunilkumarhs974117@gmail.com>
2024-12-04 02:47:14 +0530

    glabal gile cloading(

---
+++ src/app/dashboard/[fileid]/page.tsx
@@ -0,0 +1,30 @@
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound, redirect } from "next/navigation";
+
+interface PageProps {
+  params: {
+    fileid: string;
+  };
+}
+
+const Page = async ({ params }: PageProps) => {
+  const { fileid } = params;
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || !user) redirect(`/auth-callback?origin=dashboard/${fileid}`);
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileid,
+      userId: user.id,
+    },
+  });
+  if (!file) notFound();
+
+  return;
+};
+
+export default Page;
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,9 +5,29 @@ import UploadButton from "./UploadButton";
 import Image from "next/image";
 
 import Skeleton from "react-loading-skeleton";
+import Link from "next/link";
+import { LoaderPinwheel, MessageSquare, Plus, Trash2 } from "lucide-react";
+import { format } from "date-fns";
+import { Button } from "./ui/button";
+import { useState } from "react";
 
 const Dashboard = () => {
+  const [currentlyDeletingFile, setCurrentlyDeletingFile] = useState<
+    string | null
+  >(null);
+  const utils = trpc.useContext();
   const { data: files, isLoading } = trpc.getUserFiles.useQuery();
+  const { mutate: deleteFile } = trpc.deleteFile.useMutation({
+    onSuccess: () => {
+      utils.getUserFiles.invalidate();
+    },
+    onMutate: ({ id }) => {
+      setCurrentlyDeletingFile(id);
+    },
+    onSettled: () => {
+      setCurrentlyDeletingFile(null);
+    },
+  });
 
   return (
     <main className="mx-auto max-w-7xl md:p-10">
@@ -19,7 +39,59 @@ const Dashboard = () => {
       {/* displaying user files */}
 
       {files && files?.length !== 0 ? (
-        <div></div>
+        <ul className="mt-8 grid grid-cols-1 gap-6 divide-y divide-stone-200 md:grid-cols-2 lg:grid-cols-3">
+          {files
+            .sort(
+              (a, b) =>
+                new Date(b.createdAt).getTime() -
+                new Date(a.createdAt).getTime()
+            )
+            .map((file) => (
+              <li
+                key={file.id}
+                className="col-span-1 divide-y divide-stone-200 rounded-lg bg-white shadow transition hover:shadow-lg"
+              >
+                <Link
+                  href={`/dashboard/${file.id}`}
+                  className="flex flex-col gap-2"
+                >
+                  <div className="pt-6 px-6 flex w-full items-center justify-between space-x-6">
+                    <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
+                    <div className="flex-1 truncate">
+                      <div className="flex items-center space-x-3">
+                        <h3 className="truncate text-lg font-medium text-stone-900">
+                          {file.name}
+                        </h3>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+
+                <div className="px-6 mt-4 grid grid-cols-3 place-items-center py-2 text-xs gap-6 text-stone-500">
+                  <div className="flex items-center gap-2">
+                    <Plus className="h-4 w-4" />
+                    {format(new Date(file.createdAt), "dd MMM yyyy")}
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <MessageSquare className="h-4 w-4" />
+                    mocked
+                  </div>
+                  <Button
+                    size="sm"
+                    className="w-full"
+                    variant="destructive"
+                    onClick={() => deleteFile({ id: file.id })}
+                  >
+                    {currentlyDeletingFile === file.id ? (
+                      <LoaderPinwheel className="h-5 w-5 animate-spin" />
+                    ) : (
+                      <Trash2 className="h-5 w-5" />
+                    )}
+                  </Button>
+                </div>
+              </li>
+            ))}
+        </ul>
       ) : isLoading ? (
         <Skeleton height={100} className="my-2" count={4} />
       ) : (
